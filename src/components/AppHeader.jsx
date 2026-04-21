bdab7a87449760b5122effcac8e9cb3315a55591
gialli24 <a.gialli2409@gmail.com>
2026-04-21 23:34:01 +0200

    add header style

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -2,10 +2,36 @@ import { NavLink } from "react-router-dom";
 
 export default function AppHeader() {
     return (
-        <header>
-            Header
+        <header className="app-header">
+            <div className="container">
+                <div className="navbar navbar-expand-lg">
 
-            <NavLink to="/">Movies</NavLink>
+                    <div className="d-flex flex-column flex-lg-row justify-content-between align-items-center gap-2 w-100">
+
+                        <div className="d-flex justify-content-between align-items-center gap-2 w-100">
+
+                            <h1>MoviesApp</h1>
+
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                        </div>
+
+                        <div className="collapse navbar-collapse justify-content-end gap-5 w-100" id="menu">
+
+                            <nav className="mb-4 mb-lg-0">
+                                <NavLink to="/" className="app-nav-link">Home</NavLink>
+                            </nav>
+
+                            <form className="d-flex" role="search" onSubmit={(e) => e.preventDefault()}>
+                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                                <button className="btn btn-primary" type="submit">Search</button>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </header>
-    )
+    );
 }
