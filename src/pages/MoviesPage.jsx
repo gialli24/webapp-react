9e4c3fd206add8dc1cc0aeb35b819659e33d162f
gialli24 <a.gialli2409@gmail.com>
2026-04-22 10:20:18 +0200

    add main page style

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
 
 export default function MoviesPage() {
 
@@ -14,16 +15,52 @@ export default function MoviesPage() {
     }, [])
 
     return (
-        <>
-            <h1>Movies Page</h1>
+        <main>
+            <section id="movies-list">
+                <div className="container pt-5">
+                    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4">
+                        {
+                            movies.map(movie => {
 
-            <ul>
-                {
-                    movies.map(movie => (
-                        <li key={movie.id}>{movie.title}</li>
-                    ))
-                }
-            </ul>
-        </>
+                                const { id, image, title, description, release_date, duration } = movie;
+                                const releaseYear = new Date(release_date).getFullYear();
+
+                                return (<div className="col" key={id}>
+                                    <div className="movie-img">
+                                        <img src={import.meta.env.VITE_API_SERVER_ADDRESS + "/img/" + image} alt={title} />
+                                    </div>
+
+                                    <div className="mt-2">
+
+                                        <div className="d-flex justify-content-between">
+                                            <span>
+                                                {releaseYear}
+                                            </span>
+                                            <span>
+                                                {duration} min
+                                            </span>
+                                        </div>
+
+                                        <h4>
+                                            {title}
+                                        </h4>
+
+                                        <p>
+                                            {description}
+                                        </p>
+
+                                        <Link to={`/movies/${id}`} className="btn btn-sm btn-outline-primary">
+                                            Dettagli
+                                        </Link>
+
+                                    </div>
+
+                                </div>)
+                            })
+                        }
+                    </div>
+                </div>
+            </section>
+        </main>
     )
 }
