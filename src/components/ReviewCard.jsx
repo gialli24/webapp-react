44aa23f910e64132f5c99d216e468dd0c90af8d7
gialli24 <a.gialli2409@gmail.com>
2026-04-22 17:23:53 +0200

    add stars instead of vote in details page

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,9 +1,22 @@
 export default function ReviewCard({ review }) {
+
+    const stars = [];
+
+    for (let i = 0; i < 5; i++) {
+        if (i < review.vote) {
+            stars.push(<i className="bi bi-star-fill" key={i}></i>);
+        } else {
+            stars.push(<i className="bi bi-star" key={i}></i>);
+        }
+    }
+
     return (
         <div className="col-md-6" key={review.id}>
             <div className="card">
                 <div className="card-body">
-                    <h5 className="card-title">{review.vote}/5</h5>
+                    <h5 className="card-title">
+                        {stars}
+                    </h5>
                     <p>{review.review}</p>
                 </div>
             </div>
