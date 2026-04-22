59132db1fc10efcf3edf7b176337a375bbfd5bf2
gialli24 <a.gialli2409@gmail.com>
2026-04-22 16:15:48 +0200

    refactor MoviesPage to use MovieCard component for rendering movie details

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
 
 export default function MoviesPage() {
 
@@ -22,40 +22,9 @@ export default function MoviesPage() {
                         {
                             movies.map(movie => {
 
-                                const { id, image, title, description, release_date, duration } = movie;
-                                const releaseYear = new Date(release_date).getFullYear();
-
-                                return (<div className="col" key={id}>
-                                    <div className="movie-img">
-                                        <img src={import.meta.env.VITE_API_SERVER_ADDRESS + "/img/" + image} alt={title} />
-                                    </div>
-
-                                    <div className="mt-2">
-
-                                        <div className="d-flex justify-content-between">
-                                            <span>
-                                                {releaseYear}
-                                            </span>
-                                            <span>
-                                                {duration} min
-                                            </span>
-                                        </div>
-
-                                        <h4>
-                                            {title}
-                                        </h4>
-
-                                        <p>
-                                            {description}
-                                        </p>
-
-                                        <Link to={`/movies/${id}`} className="btn btn-sm btn-outline-primary">
-                                            Dettagli
-                                        </Link>
-
-                                    </div>
-
-                                </div>)
+                                return (
+                                    <MovieCard key={movie.id} movie={movie} />
+                                )
                             })
                         }
                     </div>
